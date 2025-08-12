c5c1c2d68c2dde71c95e33abec4c96bf385e5749
WonJeong Kim (김원정) <circlejj@pukyong.ac.kr>
2025-08-12 21:12:46 +0900

    Create docking_Mission.launch.py

---
+++ src/docking/docking_Mission.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='pass_docking',
+            executable='docking_Cont',
+            name='docking_Cont',
+            output='screen'
+        ),
+        Node(
+            package='pass_docking',
+            executable='docking_Guid',
+            name='docking_Guid',
+            output='screen'
+        ),
+        Node(
+            package='pass_docking',
+            executable='docking_Navi',
+            name='docking_Navi',
+            output='screen'
+        )
+    ])
